13bff3669b8af9b17e73c27b5ed8b0dd6c2c1210
eguns <egunseli4@gmail.com>
2022-10-28 05:50:57 +0100

    Coding Complete (Except for sounds and optimisation)

---
+++ tilesets/characters/characters.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="characters" tilewidth="256" tileheight="256" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="demon.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="knight.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="skeleton.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="warrior.png"/>
+ </tile>
+</tileset>
---
+++ tilesets/items/items.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="items" tilewidth="256" tileheight="256" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="217" height="256" source="axe.png"/>
+ </tile>
+ <tile id="1">
+  <image width="149" height="256" source="bone.png"/>
+ </tile>
+ <tile id="2">
+  <image width="146" height="256" source="club.png"/>
+ </tile>
+ <tile id="3">
+  <image width="112" height="256" source="fork.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="greater_healing.png"/>
+ </tile>
+ <tile id="5">
+  <image width="144" height="256" source="hammer.png"/>
+ </tile>
+ <tile id="6">
+  <image width="85" height="256" source="spear.png"/>
+ </tile>
+ <tile id="7">
+  <image width="48" height="256" source="stick.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="super_healing.png"/>
+ </tile>
+ <tile id="9">
+  <image width="102" height="256" source="sword.png"/>
+ </tile>
+ <tile id="10">
+  <image width="138" height="256" source="trident.png"/>
+ </tile>
+ <tile id="11">
+  <image width="149" height="256" source="nun_chucks.png"/>
+ </tile>
+ <tile id="12">
+  <image width="171" height="256" source="rapier.png"/>
+ </tile>
+ <tile id="13">
+  <image width="110" height="256" source="katana.png"/>
+ </tile>
+ <tile id="14">
+  <image width="67" height="256" source="lance.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="256" source="lesser_healing.png"/>
+ </tile>
+ <tile id="16">
+  <image width="112" height="256" source="longsword.png"/>
+ </tile>
+ <tile id="17">
+  <image width="256" height="256" source="normal_healing.png"/>
+ </tile>
+</tileset>
